16866096bcd830c857123eb22526cbb42433a5e6
devfernandoabreu <devfernandoabreu@gmail.com>
2021-01-19 18:06:56 -0300

    Ajuste cors

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -16,11 +16,9 @@ import '@shared/container';
 
 const app = express();
 
-app.use(
-  cors({
+app.use(cors(/* {
     origin: 'http://localhost:3000',
-  }),
-);
+  } */));
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
 app.use(rateLimiter);
